9638f35f478beaf0c2f99ca27ef4e2424e246981
Aswun <aswinlutfian32@gmail.com>
2025-11-01 20:38:11 +0700

    menambahkan variabel status option

--- app/src/main/java/com/example/prak5/TeksEdit.kt
+++ app/src/main/java/com/example/prak5/TeksEdit.kt
@@ -42,6 +42,7 @@ fun FormDataDiri(modifier: Modifier) {
     var jenis by remember { mutableStateOf("") }
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
+    val statusOptions: List<String> = listOf("Janda", "Lajang", "Duda")
 
     Column(
         modifier = modifier.padding(top = 50.dp),
